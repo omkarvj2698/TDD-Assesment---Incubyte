561a6d79b4bc1b68e8046b8c6dbcce0f6ed6137e
omkarvj2698 <omkarvj2698@gmail.com>
2025-10-02 10:02:09 +0530

    add test for newline as delimiter

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,21 @@
 from calculator import Calculator
 
+def test_placeholder():
+    calc = Calculator()
+    assert isinstance(calc, Calculator)
+
+def test_empty_string_returns_0():
+    calc = Calculator()
+    assert calc.add("") == 0
+
+def test_single_number_returns_value():
+    calc = Calculator()
+    assert calc.add("7") == 7
+
 def test_two_numbers_are_summed():
     calc = Calculator()
     assert calc.add("1,5") == 6
+
+def test_newline_as_delimiter():
+    calc = Calculator()
+    assert calc.add("1\n2,3") == 6
